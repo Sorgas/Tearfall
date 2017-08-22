24ce99c8c9964cf8f354908310ea48be42a3ca41
Alexander <shtok47@yandex.ru>
2017-08-23 00:50:57 +0300

    localgen diamond square

--- core/src/stonering/generators/localgen/LocalHeightsGenerator.java
+++ core/src/stonering/generators/localgen/LocalHeightsGenerator.java
@@ -2,7 +2,6 @@ package stonering.generators.localgen;
 
 import stonering.game.core.model.LocalMap;
 import stonering.generators.worldgen.WorldMap;
-import stonering.utils.Position;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -18,7 +17,7 @@ public class LocalHeightsGenerator {
     private int y;
     private int xSize;
     private int ySize;
-    private int squreSize = 48;
+    private int squareSize = 48;
     private int[][] globalElevation;
 
     public LocalHeightsGenerator(WorldMap worldMap) {
@@ -45,81 +44,83 @@ public class LocalHeightsGenerator {
                 localElavation = Math.max(localElavation, worldMap.getElevation(x, y));
             }
         }
-        localMap = new LocalMap(xSize * squreSize + 1, ySize * squreSize + 1, localElavation * config.getWorldToLocalElevationModifier() + 30);
+        localMap = new LocalMap(xSize * squareSize + 1, ySize * squareSize + 1, localElavation * config.getWorldToLocalElevationModifier() + 30);
     }
 
     private void generateHights(int x, int y, int lx, int ly) {
         //calculating corners
-        int[][] localHights = new int[squreSize + 1][squreSize + 1];
+        int[][] localHights = new int[squareSize + 1][squareSize + 1];
         Arrays.fill(localHights, -1);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                localHights[i * squreSize][j * squreSize] = calculateMidElevation(x + i, y + j);
+                localHights[i * squareSize][j * squareSize] = calculateMidElevation(x + i, y + j);
             }
         }
         //calculating borders
         recursiveMidpoint(localHights[0], 0, localHights[0].length, new Random(x * y + 1));
-        recursiveMidpoint(localHights[squreSize], 0, localHights[squreSize].length, new Random((x + 1) * y + 1));
-        int[] border = new int[squreSize + 1];
-        for (int i = 0; i < squreSize + 1; i++) {
+        recursiveMidpoint(localHights[squareSize], 0, localHights[squareSize].length, new Random((x + 1) * y + 1));
+        int[] border = new int[squareSize + 1];
+        for (int i = 0; i < squareSize + 1; i++) {
             border[i] = localHights[i][0];
         }
         recursiveMidpoint(border, 0, border.length, new Random(x * y));
-        for (int i = 0; i < squreSize + 1; i++) {
+        for (int i = 0; i < squareSize + 1; i++) {
             localHights[i][0] = border[i];
-            border[i] = localHights[i][squreSize];
+            border[i] = localHights[i][squareSize];
         }
         recursiveMidpoint(border, 0, border.length, new Random(x * (y + 1)));
-        for (int i = 0; i < squreSize + 1; i++) {
-            localHights[i][squreSize] = border[i];
+        for (int i = 0; i < squareSize + 1; i++) {
+            localHights[i][squareSize] = border[i];
+        }
+        while (squareSize > 6) {
+            performSquare(localHights, squareSize);
+            performDiamond(localHights, squareSize);
+            squareSize /= 2;
         }
 
-        performSquare(localHights, squreSize);
 
     }
 
     private void performSquare(int[][] array, int step) {
-        for (int x = 0; x < array.length; x+=step) {
-            for (int y = 0; y < array[0].length; y+=step) {
-                float midValue = array[x * step][y * step];
-                midValue += array[(x + 1) * step][y * step];
-                midValue += array[x * step][(y + 1) * step];
-                midValue += array[(x + 1) * step][(y + 1) * step];
-                array[x * step + step / 2][y * step + step / 2] = (int) (midValue / 4);
+        for (int x = 0; x < array.length; x += step) {
+            for (int y = 0; y < array[0].length; y += step) {
+                float midValue = array[x][y];
+                midValue += array[x + step][y];
+                midValue += array[x][y + step];
+                midValue += array[x + step][y + step];
+                array[x + step / 2][y + step / 2] = (int) (midValue / 4);
             }
         }
     }
 
     private void performDiamond(int[][] array, int step) {
-        for (int x = 0; x < array.length; x+=step) {
-            for (int y = 0; y < array[0].length + 1; y+=step) {
-                if (x < numX) {
-                    float midValue = array[x * step][y * step];
-                    midValue += array[(x + 1) * step][y * step];
-                    int count = 2;
-                    if (y > 0) {
-                        midValue += array[x * step + step / 2][y * step - step / 2];
-                        count++;
-                    }
-                    if (y < numY - 1) {
-                        midValue += array[x * step + step / 2][y * step + step / 2];
-                        count++;
-                    }
-                    array[x * step + step / 2][y * step] = midValue / count;
+        for (int x = 0; x < array.length - 1; x += step) {
+            for (int y = 0; y < array[0].length - 1; y += step) {
+                if (x + step < array.length - 1 && y > 0 && y < array[0].length - 1) {
+                    int midValue = array[x][y];
+                    midValue += array[x + step][y];
+                    midValue += array[x + step / 2][y - step / 2];
+                    midValue += array[x + step / 2][y + step / 2];
+                    array[x + step / 2][y] = midValue / 4;
                 }
-                if (y < numY) {
-                    float midValue = array[x * step][y * step];
-                    midValue += array[x * step][(y + 1) * step];
-                    int count = 2;
-                    if (x > 0) {
-                        midValue += array[x * step - step / 2][y * step + step / 2];
-                        count++;
+                if (y + step < array[0].length - 1 && x > 0 && x < array.length - 1) {
+                    int midValue = array[x][y];
+                    midValue += array[x][y + step];
+                    midValue += array[x + step / 2][y + step / 2];
+                    midValue += array[x - step / 2][y + step / 2];
+                    array[x][y + step / 2] = midValue / 4;
+                }
+            }
+        }
+    }
+
+    private void fillHeights(int[][] array, int step) {
+        for (int x = 0; x < array.length - 1; x += step) {
+            for (int y = 0; y < array[0].length - 1; y += step) {
+                for (int i = 0; i < step; i++) {
+                    for (int j = 0; j < step; j++) {
+                        
                     }
-                    if (x < numX - 1) {
-                        midValue += array[x * step + step / 2][y * step + step / 2];
-                        count++;
-                    }
-                    array[x * step][y * step + step / 2] = midValue / count;
                 }
             }
         }
