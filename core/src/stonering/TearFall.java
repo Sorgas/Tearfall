20e25fcde47ef72a60cf04f848f48a344bbfb5e7
Alexander Kuzyakov <alexander.kuzyakov@gmail.com>
2018-06-05 14:08:29 +0300

    prepare expedition menu reduced to single class

--- core/src/stonering/TearFall.java
+++ core/src/stonering/TearFall.java
@@ -9,88 +9,89 @@ import stonering.game.core.GameMvc;
 import stonering.generators.localgen.LocalGenContainer;
 import stonering.menu.main.MainMenu;
 import stonering.menu.new_game.local_generation.LocalGenerationScreen;
-import stonering.menu.new_game.prepare_expedition.PrepareExpeditionMenuMvc;
+import stonering.menu.new_game.prepare_expedition.PrepareExpeditionMenu;
 import stonering.menu.new_game.select_location.SelectLocationMenuMvc;
 import stonering.menu.new_game.select_world.SelectWorldMenu;
 import stonering.menu.worldgen.WorldGenMvc;
 import stonering.generators.worldgen.WorldMap;
 import stonering.global.utils.Position;
 
-/**s
+/**
+ * s
  * Created by Alexander on 08.04.2017.
  */
 public class TearFall extends Game {
-	private MainMenu mainMenu;
-	private WorldGenMvc worldGenMvc;
-	private SelectWorldMenu selectWorldMenu;
-	private SelectLocationMenuMvc selectLocationMenuMvc;
-	private PrepareExpeditionMenuMvc prepareExpeditionMenuMvc;
-	private LocalGenerationScreen localGenerationScreen;
-	private GameMvc gameMvc;
+    private MainMenu mainMenu;
+    private WorldGenMvc worldGenMvc;
+    private SelectWorldMenu selectWorldMenu;
+    private SelectLocationMenuMvc selectLocationMenuMvc;
+    private PrepareExpeditionMenu prepareExpeditionMenuMvc;
+    private LocalGenerationScreen localGenerationScreen;
+    private GameMvc gameMvc;
 
-	private BitmapFont font;
-	private Skin skin;
+    private BitmapFont font;
+    private Skin skin;
 
-	@Override
-	public void create() {
-		createFont();
-		createSkin();
-		switchMainMenu();
-	}
+    @Override
+    public void create() {
+        createFont();
+        createSkin();
+        switchMainMenu();
+    }
 
-	public void switchMainMenu() {
-		setScreen(mainMenu != null ? mainMenu : new MainMenu(this));
-	}
+    public void switchMainMenu() {
+        setScreen(mainMenu != null ? mainMenu : new MainMenu(this));
+    }
 
-	public void switchWorldGenMenu() {
-		if(worldGenMvc == null) worldGenMvc = new WorldGenMvc(this);
-		setScreen(worldGenMvc.getView());
-	}
+    public void switchWorldGenMenu() {
+        if (worldGenMvc == null) worldGenMvc = new WorldGenMvc(this);
+        setScreen(worldGenMvc.getView());
+    }
 
-	public void switchWorldsSelectMenu() {
-		setScreen(selectWorldMenu != null ? selectWorldMenu : new SelectWorldMenu(this));
-	}
+    public void switchWorldsSelectMenu() {
+        setScreen(selectWorldMenu != null ? selectWorldMenu : new SelectWorldMenu(this));
+    }
 
-	public void switchLocationSelectMenu(WorldMap world) {
-		if(selectLocationMenuMvc == null) selectLocationMenuMvc = new SelectLocationMenuMvc(this);
-		selectLocationMenuMvc.getModel().setWorld(world);
-		setScreen(selectLocationMenuMvc.getView());
-	}
+    public void switchLocationSelectMenu(WorldMap world) {
+        if (selectLocationMenuMvc == null) selectLocationMenuMvc = new SelectLocationMenuMvc(this);
+        selectLocationMenuMvc.getModel().setWorld(world);
+        setScreen(selectLocationMenuMvc.getView());
+    }
 
-	public void switchPrepareExpeditionMenu(WorldMap world, Position location) {
-		if(prepareExpeditionMenuMvc == null) prepareExpeditionMenuMvc = new PrepareExpeditionMenuMvc(this);
-		prepareExpeditionMenuMvc.getModel().setWorld(world);
-		prepareExpeditionMenuMvc.getModel().setLocation(location);
-		setScreen(prepareExpeditionMenuMvc.getView());
-	}
+    public void switchPrepareExpeditionMenu(WorldMap world, Position location) {
+        if (prepareExpeditionMenuMvc == null) prepareExpeditionMenuMvc = new PrepareExpeditionMenu(this);
+        prepareExpeditionMenuMvc.setWorld(world);
+        prepareExpeditionMenuMvc.setLocation(location);
+        setScreen(prepareExpeditionMenuMvc);
+    }
 
-	public void switchToLocalGen(WorldMap world, Position location) {
-		if(localGenerationScreen == null) localGenerationScreen = new LocalGenerationScreen(this);
-		localGenerationScreen.setWorld(world);
-		localGenerationScreen.setLocation(location);
-		setScreen(localGenerationScreen);
-	}
+    public void switchToLocalGen(WorldMap world, Position location) {
+        if (localGenerationScreen == null) localGenerationScreen = new LocalGenerationScreen(this);
+        localGenerationScreen.setWorld(world);
+        localGenerationScreen.setLocation(location);
+        setScreen(localGenerationScreen);
+    }
 
-	public void switchToGame(LocalGenContainer container) {
-		gameMvc = new GameMvc(container);
-		setScreen(gameMvc.getView());
-	}
+    public void switchToGame(LocalGenContainer container) {
+        gameMvc = new GameMvc(container);
+        setScreen(gameMvc.getView());
+    }
 
-	private void createFont() {
-		font = new BitmapFont();
-		font.setColor(0.2f, 0.2f, 0.2f, 1);
-	}
+    private void createFont() {
+        font = new BitmapFont();
+        font.setColor(0.2f, 0.2f, 0.2f, 1);
+    }
 
-	private void createSkin() {
-		TextureAtlas atlas = new TextureAtlas(new FileHandle("ui_skin/uiskin.atlas"));
-		skin = new Skin(new FileHandle("ui_skin/uiskin.json"), atlas);
-	}
+    private void createSkin() {
+        TextureAtlas atlas = new TextureAtlas(new FileHandle("ui_skin/uiskin.atlas"));
+        skin = new Skin(new FileHandle("ui_skin/uiskin.json"), atlas);
+    }
 
-	public BitmapFont getFont() {
-		return font;
-	}
+    public BitmapFont getFont() {
+        return font;
+    }
 
-	public Skin getSkin() {
-		return skin;
-	}
+    public Skin getSkin() {
+        return skin;
+    }
 }
---
+++ core/src/stonering/menu/new_game/prepare_expedition/PrepareExpeditionMenu.java
@@ -0,0 +1,150 @@
+package stonering.menu.new_game.prepare_expedition;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
+import stonering.TearFall;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Array;
+import stonering.generators.worldgen.WorldMap;
+import stonering.menu.ui_components.WorldListItem;
+import stonering.global.utils.Position;
+
+import java.io.File;
+
+/**
+ * Created by Alexander on 14.04.2017.
+ */
+public class PrepareExpeditionMenu implements Screen {
+    private TearFall game;
+    private Stage stage;
+    private WorldMap world;
+    private Position location;
+
+    public PrepareExpeditionMenu(TearFall game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+        stage = new Stage();
+        init();
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT | Gdx.gl20.GL_DEPTH_BUFFER_BIT);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.dispose();
+        stage = new Stage();
+        init();
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    public Array<WorldListItem> getWorldListItems() {
+        File root = new File("saves");
+        Array<WorldListItem> list = new Array<>();
+        for (File file : root.listFiles()) {
+            list.add(new WorldListItem(file.getName(), file));
+        }
+        return list;
+    }
+
+    private void init() {
+        stage.setDebugAll(true);
+        Table rootTable = new Table();
+        rootTable.setFillParent(true);
+        rootTable.defaults().fill().expandY().space(10);
+        rootTable.pad(10).align(Align.bottomLeft);
+        rootTable.add(createMenuTable());
+        stage.addActor(rootTable);
+    }
+
+    private Table createMenuTable() {
+        Table menuTable = new Table();
+        menuTable.defaults().prefHeight(30).prefWidth(300).padBottom(10).minWidth(300);
+        menuTable.align(Align.bottomLeft);
+
+        menuTable.add(new Label("Prepare to advance", game.getSkin())).row();
+
+        menuTable.add().expandY();
+        menuTable.row();
+
+        TextButton proceedButton = new TextButton("Start", game.getSkin());
+        proceedButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.switchToLocalGen(getWorld(), getLocation());
+            }
+        });
+        menuTable.add(proceedButton);
+        menuTable.row();
+
+        TextButton backButton = new TextButton("Back", game.getSkin());
+        backButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.switchLocationSelectMenu(getWorld());
+            }
+        });
+        menuTable.add(backButton).colspan(2).pad(0);
+
+        return menuTable;
+    }
+
+    public void checkInput() {
+
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setWorld(WorldMap world) {
+        this.world = world;
+    }
+
+    public WorldMap getWorld() {
+        return world;
+    }
+
+    public void setLocation(Position location) {
+        this.location = location;
+    }
+
+    public Position getLocation() {
+        return location;
+    }
+}
