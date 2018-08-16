13454c3417dc0ceb07ab6b54e075c1ef25e8fac0
Alexander Kuzyakov <alexander.kuzyakov@gmail.com>
2018-08-16 18:39:09 +0300

    add InFlow class to LocalRiverGenerator

--- core/src/stonering/generators/localgen/generators/LocalRiverGenerator.java
+++ core/src/stonering/generators/localgen/generators/LocalRiverGenerator.java
@@ -26,9 +26,10 @@ public class LocalRiverGenerator {
     private Position location;
     private ArrayList<Position> incomingRivers; // positions on local map
     private ArrayList<Position> incomingBrooks; // positions on local map
-    private Vector2 currentFlow; // direction of current flow
+    private ArrayList<Inflow> inflows;
+    private Inflow currentFlow; // direction of current flow
     private boolean currentFlowIsRiver;
-    private Position mainInflow; // position on local map
+    private Inflow mainInflow; // position on local map
     private MaterialMap materialMap;
 
     public LocalRiverGenerator(LocalGenContainer container) {
@@ -52,8 +53,7 @@ public class LocalRiverGenerator {
         worldMap = container.getWorldMap();
         localMap = container.getLocalMap();
         location = container.getConfig().getLocation();
-        incomingBrooks = new ArrayList<>();
-        incomingRivers = new ArrayList<>();
+        inflows = new ArrayList<>();
         localMap = container.getLocalMap();
         materialMap = MaterialMap.getInstance();
     }
@@ -63,13 +63,9 @@ public class LocalRiverGenerator {
      */
     private void makeMainFlow() {
         LandscapeBrush brush = new LandscapeBrush(new ArrayList<>(Arrays.asList(5, 3, 2)), 1);
-        Position startPos = getPositionOnBorderByWorldCoords(mainInflow);
-        Vector2 start = new Vector2(startPos.getX(), startPos.getY());
 
-        Vector2 end = currentFlow.cpy().nor();
-        Position endPos = getPositionOnBorderByWorldCoords(new Position(Math.round(end.x), Math.round(end.y), 0));
-        end = new Vector2(endPos.getX(), endPos.getY());
-
+        Vector2 start = new Vector2(mainInflow.localStart.getX(), mainInflow.localStart.getY());
+        Vector2 end = new Vector2(currentFlow.localStart.getX(), currentFlow.localStart.getY());
         Vector2 center = new Vector2(localMap.getxSize() / 2, localMap.getySize() / 2);
 
         Vector2[] vectors = {start, center, end};
@@ -80,8 +76,8 @@ public class LocalRiverGenerator {
 
     private void makeRiverStart() {
         LandscapeBrush brush = new LandscapeBrush(new ArrayList<>(Arrays.asList(5, 3, 2)), 1);
-        currentFlow.nor();
-        Vector2[] controlPoints = {new Vector2(localMap.getxSize() / 2, localMap.getySize() / 2), new Vector2(Math.round(currentFlow.x), Math.round(currentFlow.y))};
+        Vector2[] controlPoints = {new Vector2(localMap.getxSize() / 2, localMap.getySize() / 2),
+                new Vector2(currentFlow.localStart.getX(), currentFlow.localStart.getY())};
         CatmullRomSpline<Vector2> spline = new CatmullRomSpline<>(controlPoints, false);
         carveWithBrush(spline, brush);
     }
@@ -128,87 +124,66 @@ public class LocalRiverGenerator {
      * Fills collections of inflows and sets current flow.
      */
     private void lookupFlows() {
-        int commonOffset = -1;
         int cx = location.getX();
         int cy = location.getY();
-        for (int x = cx - 1; x < cx + 2; x++) {
-            for (int y = cy - 1; y < cy + 2; y++) {
-                if ((x != cx || y != cy) && worldMap.inMap(x, y)) {
-                    if (worldMap.getRiver(x, y) != null) {
-                        Vector2 river = worldMap.getRiver(x,y).cpy().nor();
-                        if(river.x)
+        for (int dx = -1; dx < 2; dx++) { //offset x
+            int x = cx + dx; //absolute x
+            for (int dy = -1; dy < 2; dy++) { //offset y
+                int y = cy + dy; // absolute y
+                if ((x != cx || y != cy) && worldMap.inMap(x, y)) { //not current point and not out of borders
+                    if (worldMap.getRiver(x, y) != null && isInflow(dx, dy, worldMap.getRiver(x, y))) {
+                        inflows.add(createInflow(dx, dy, true));
+                    }
+                    if (worldMap.getBrook(x, y) != null && isInflow(dx, dy, worldMap.getBrook(x, y))) {
+                        inflows.add(createInflow(dx, dy, false));
                     }
                 }
             }
         }
-//        for (int dx = 0; dx < 3; dx++) {
-//            int x = dx + commonOffset + cx;
-//            for (int dy = 0; dy < 3; dy++) {
-//                int y = dy + commonOffset + cy;
-//
-//            }
-//        }
-        for (int dx = -1; dx < 2; dx++) {
-            for (int dy = -1; dy < 2; dy++) {
-                int x = cx + dx;
-                int y = cy + dy;
-                if ((dx != 0 || dy != 0) && worldMap.inMap(x, y)) {
-                    if (worldMap.getRiver(x, y) != null && isInflow(dx, dy, worldMap.getRiver(x, y).cpy())) {
-                        incomingRivers.add(new Position(x, y, 0));
-                    }
-                    if (worldMap.getBrook(x, y) != null && isInflow(dx, dy, worldMap.getBrook(x, y).cpy())) {
-                        incomingBrooks.add(new Position(x, y, 0));
-                    }
-                }
-            }
+        if (worldMap.getRiver(cx, cy) != null) {
+            IntVector2 intVector = new IntVector2(worldMap.getRiver(cx, cy)); // pointing out
+            this.currentFlow = createInflow(intVector.x, intVector.y, true); // localStart is end of flow
+        } else if (worldMap.getBrook(cx, cy) != null) {
+            IntVector2 intVector = new IntVector2(worldMap.getBrook(cx, cy)); // pointing out
+            this.currentFlow = createInflow(intVector.x, intVector.y, false); // localStart is end of flow
         }
-        currentFlow = worldMap.getRiver(cx, cy);
-        if (currentFlow != null) {
-            currentFlowIsRiver = true;
-        } else {
-            currentFlow = worldMap.getBrook(cx, cy);
-            currentFlowIsRiver = currentFlow == null;
-        }
-        System.out.println("incoming rivers: " + incomingRivers.size());
-        System.out.println("incoming brooks: " + incomingBrooks.size());
-        System.out.println("currentFlow: " + currentFlow.toString());
-        System.out.println("current flow is river: " + currentFlowIsRiver);
+        System.out.println("inflows: " + inflows.size());
     }
 
     /**
      * Checks if given vector points to current position.
      *
-     * @param relativeX
-     * @param relativeY
-     * @param vector
+     * @param rx
+     * @param ry
+     * @param inflow
      * @return
      */
+    private boolean isInflow(int rx, int ry, Vector2 inflow) {
+        System.out.println("inflow check: " + rx + " " + ry + " " + inflow);
+        System.out.println("vector: " + Math.round(inflow.x) + " " + Math.round(inflow.y));
+        IntVector2 intVector = new IntVector2(inflow);
+        return intVector.x == -rx && intVector.y == -ry; // inflow vector points from observed position to current. [rx, ry] is offset from current position to position where inflow comes from.
+    }
 
-    private boolean isInflow(int relativeX, int relativeY, Vector2 vector) {
-        System.out.println("inflow check: " + relativeX + " " + relativeY + " " + vector);
-        System.out.println("vector: " + Math.round(vector.x) + " " + Math.round(vector.y));
-        vector.nor();
-        return Math.round(vector.x) + relativeX == 0 && Math.round(vector.y) + relativeY == 0; // vector points to current location
+    private Inflow createInflow(int dx, int dy, boolean river) {
+        Inflow inflow = new Inflow();
+        inflow.isRiver = river;
+        inflow.offset = new Position(dx, dy, 0);
+        inflow.localStart = new Position((dx + 1) * (localMap.getxSize() / 2), (dy + 1) * (localMap.getySize() / 2), 0);
+        if (river) {
+            inflow.waterAmount = worldMap.getRiver(location.getX() + dx, location.getY() + dy).len();
+        } else {
+            inflow.waterAmount = worldMap.getBrook(location.getX() + dx, location.getY() + dy).len();
+        }
+        return inflow;
     }
 
     private void determineMailInflow() {
-        if (currentFlowIsRiver || !incomingRivers.isEmpty()) {
-            // if current flow is river, only river inflow can be main.
-            // if current flow is brook and river inflows present, one of them should be main as well.
-            incomingRivers.forEach(position -> {
-                if (mainInflow == null || // first found inflow will go
-                        worldMap.getRiver(position.getX(), position.getY()).len() > worldMap.getRiver(mainInflow.getX(), mainInflow.getY()).len()) { // choose inflow with greater amount of water
-                    mainInflow = position;
-                }
-            });
-        } else { // no rivers, look for biggest brook
-            incomingBrooks.forEach(position -> {
-                if (mainInflow == null || // first found inflow will go
-                        worldMap.getBrook(position.getX(), position.getY()).len() > worldMap.getBrook(mainInflow.getX(), mainInflow.getY()).len()) { // choose inflow with greater amount of water
-                    mainInflow = position;
-                }
-            });
-        }
+        inflows.forEach(inflow -> {
+            if (mainInflow == null || mainInflow.waterAmount < inflow.waterAmount) {
+                mainInflow = inflow;
+            }
+        });
     }
 
     private void carveWithBrush(CatmullRomSpline<Vector2> spline, LandscapeBrush brush) {
@@ -260,6 +235,44 @@ public class LocalRiverGenerator {
                         }
                     }
                 }
+            }
+        }
+    }
+
+    private class Inflow {
+        boolean isRiver; // or brook
+        Position offset;
+        Position localStart;
+        float waterAmount;
+    }
+
+    private class IntVector2 {
+        int x;
+        int y;
+
+        public IntVector2(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        /**
+         * Creates IntVector basing on vector angle.
+         * All angles range divided into 8 sectors corresponding to 8 neighbours on square grid.
+         * x and y become offset to neighbour pointed by vector.
+         *
+         * @param vector
+         */
+        public IntVector2(Vector2 vector) {
+            float angleSector = (float) (vector.cpy().nor().angleRad() / Math.PI) / (1f / 16f); // number of 1/16 sectors in angle of inflow vector
+            if (angleSector < 3 || angleSector >= 13) {
+                x = 1;
+            } else if (angleSector >= 5 && angleSector < 11) {
+                x = -1;
+            }
+            if (angleSector >= 1 && angleSector < 7) {
+                y = 1;
+            } else if (angleSector >= 9 && angleSector < 15) {
+                y = -1;
             }
         }
     }
