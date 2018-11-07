084ff199b1b2660f37a19cc1b83c0462a7189761
Alexander <shtok47@yandex.ru>
2018-11-07 22:44:06 +0300

    fix camera movement

--- core/src/stonering/game/core/controller/controllers/GameInputHandler.java
+++ core/src/stonering/game/core/controller/controllers/GameInputHandler.java
@@ -7,9 +7,6 @@ import stonering.game.core.GameMvc;
 import stonering.game.core.model.GameCamera;
 import stonering.game.core.view.GameView;
 import stonering.game.core.view.render.ui.components.menus.util.Invokable;
-import stonering.game.core.view.render.ui.components.menus.util.MouseInvocable;
-
-import java.util.Arrays;
 
 /**
  * Handles all input in the game.
@@ -20,6 +17,10 @@ import java.util.Arrays;
 public class GameInputHandler extends Controller implements Invokable {
     private GameView gameView;
     private GameCamera camera;
+    private boolean W;
+    private boolean A;
+    private boolean S;
+    private boolean D;
 
     public GameInputHandler(GameMvc gameMvc) {
         super(gameMvc);
@@ -36,6 +37,12 @@ public class GameInputHandler extends Controller implements Invokable {
         return gameView.invoke(Math.round(vector2.x), Math.round(vector2.y), button, action);
     }
 
+    /**
+     * Invoked on keyDown.
+     *
+     * @param keycode
+     * @return
+     */
     @Override
     public boolean invoke(int keycode) {
         if (!gameView.invoke(keycode)) { // no ui is active
@@ -44,20 +51,62 @@ public class GameInputHandler extends Controller implements Invokable {
         return true;
     }
 
-    public void tryMoveCamera(int keycode) {
+    /**
+     * On keyDown.
+     *
+     * @param keycode
+     */
+    private void tryMoveCamera(int keycode) {
         int offset = Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) ? 10 : 1;
         switch (keycode) {
             case Input.Keys.W:
                 camera.moveCamera(0, offset, 0);
-                break;
-            case Input.Keys.S:
-                camera.moveCamera(0, -offset, 0);
+                W = true;
                 break;
             case Input.Keys.A:
                 camera.moveCamera(-offset, 0, 0);
+                A = true;
+                break;
+            case Input.Keys.S:
+                camera.moveCamera(0, -offset, 0);
+                S = true;
                 break;
             case Input.Keys.D:
                 camera.moveCamera(offset, 0, 0);
+                D = true;
+                break;
+        }
+    }
+
+    public void typeCameraKey(int keycode) {
+        switch (keycode) {
+            case Input.Keys.W:
+                if (W) {
+                    W = false;
+                } else {
+                    observePressedWasd();
+                }
+                break;
+            case Input.Keys.A:
+                if (A) {
+                    A = false;
+                } else {
+                    observePressedWasd();
+                }
+                break;
+            case Input.Keys.S:
+                if (S) {
+                    S = false;
+                } else {
+                    observePressedWasd();
+                }
+                break;
+            case Input.Keys.D:
+                if (D) {
+                    D = false;
+                } else {
+                    observePressedWasd();
+                }
                 break;
             case Input.Keys.R:
                 camera.moveCamera(0, 0, 1);
@@ -65,6 +114,22 @@ public class GameInputHandler extends Controller implements Invokable {
             case Input.Keys.F:
                 camera.moveCamera(0, 0, -1);
                 break;
+        }
+    }
+
+    private void observePressedWasd() {
+        int offset = Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) ? 10 : 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            camera.moveCamera(0, offset, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            camera.moveCamera(0, -offset, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            camera.moveCamera(-offset, 0, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            camera.moveCamera(offset, 0, 0);
         }
     }
 }
--- core/src/stonering/game/core/controller/inputProcessors/GameInputProcessor.java
+++ core/src/stonering/game/core/controller/inputProcessors/GameInputProcessor.java
@@ -1,11 +1,13 @@
 package stonering.game.core.controller.inputProcessors;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
 import stonering.game.core.controller.controllers.GameController;
 import stonering.game.core.controller.controllers.GameInputHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Intercepts all game input. Sends events to {@link GameInputHandler}
@@ -16,6 +18,7 @@ public class GameInputProcessor extends DragListener implements InputProcessor {
     public final static int DOWN_CODE = 0;
     public final static int UP_CODE = 1;
     public final static int MOVE_CODE = 2;
+    private final static ArrayList<Character> cameraHorizontalMove = new ArrayList<>(Arrays.asList('a', 's', 'd', 'w', 'A', 'S', 'D', 'W','r','f','R','F'));
 
     private GameInputHandler gameInputHandler;
 
@@ -24,8 +27,17 @@ public class GameInputProcessor extends DragListener implements InputProcessor {
     }
 
     @Override
+    public boolean keyDown(int keycode) {
+        return gameInputHandler.invoke(keycode);
+    }
+
+    @Override
     public boolean keyTyped(char character) {
-        return gameInputHandler.invoke(charToKeycode(character));
+        if (cameraHorizontalMove.contains(character)) {
+            gameInputHandler.typeCameraKey(charToKeycode(character));
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -64,11 +76,6 @@ public class GameInputProcessor extends DragListener implements InputProcessor {
 
     private int charToKeycode(char character) {
         return Input.Keys.valueOf(String.valueOf(character).toUpperCase());
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
     }
 
     @Override
