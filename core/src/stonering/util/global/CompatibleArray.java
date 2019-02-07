594affd87989659dcc98f039feb6f91c021805c4
Alexander <shtok47@yandex.ru>
2019-02-07 23:59:34 +0300

    comments

--- core/src/stonering/util/global/CompatibleArray.java
+++ core/src/stonering/util/global/CompatibleArray.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.utils.Array;
 import java.util.List;
 
 /**
+ * Provides constructor from {@link List}.
+ *
  * @author Alexander on 07.02.2019.
  */
 public class CompatibleArray<T> extends Array<T> {
