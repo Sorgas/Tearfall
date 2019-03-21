acb694cee9d1bbbb07b8f0a2e57fe3e2b521bef4
Alexander_Kuzyakov <alexander_kuzyakov@epam.com>
2019-03-21 19:01:57 +0300

    q

--- core/src/stonering/game/view/render/ui/menus/zone/FarmZoneMenu.java
+++ core/src/stonering/game/view/render/ui/menus/zone/FarmZoneMenu.java
@@ -1,8 +1,7 @@
 package stonering.game.view.render.ui.menus.zone;
 
-import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import stonering.entity.local.plants.Plant;
 import stonering.util.global.StaticSkin;
 
 /**
@@ -11,19 +10,25 @@ import stonering.util.global.StaticSkin;
  * @author Alexander on 20.03.2019.
  */
 public class FarmZoneMenu extends Window {
+    private List<Plant> enabledPlants;
+    private List<Plant> disabledPlants;
+    private HorizontalGroup bottomButtons;
 
     public FarmZoneMenu() {
         super("qwer", StaticSkin.getSkin());
-
+        createTable();
     }
 
     private void createTable() {
         setDebug(true, true);
+        enabledPlants = new List<>(StaticSkin.getSkin());
 
-        add(createOrderList().fill()).prefWidth(600).prefHeight(200).expandX();
-        add(createCloseButton()).prefWidth(20).prefHeight(20).right().top().row();
+        add(enabledPlants).prefWidth(Value.percentWidth(0.5f, this))
+                .prefHeight(Value.percentHeight(0.5f, this));
+        add(disabledPlants).prefWidth(Value.percentWidth(0.5f, this))
+                .prefHeight(Value.percentHeight(0.5f, this));
 
-        HorizontalGroup horizontalGroup = new HorizontalGroup();
+        bottomButtons = new HorizontalGroup();
         horizontalGroup.addActor(createAddButton());
         horizontalGroup.addActor(hintLabel = new Label(MENU_HINT, StaticSkin.getSkin()));
         add(horizontalGroup).prefHeight(20).left().top();
