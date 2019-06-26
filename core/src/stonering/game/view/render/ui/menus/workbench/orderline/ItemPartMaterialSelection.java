d4a5615b3c07d58a322f74b4469d4e19901dd08b
Alexander_Kuzyakov <alexander_kuzyakov@epam.com>
2019-06-26 18:48:03 +0300

    add comment for order line block

--- core/src/stonering/game/view/render/ui/menus/workbench/orderline/ItemPartMaterialSelection.java
+++ core/src/stonering/game/view/render/ui/menus/workbench/orderline/ItemPartMaterialSelection.java
@@ -1,7 +1,13 @@
 package stonering.game.view.render.ui.menus.workbench.orderline;
 
+import stonering.entity.local.crafting.ItemPartOrder;
+
 /**
+ * Blocks for {@link OrderLine} for a single {@link ItemPartOrder} with a single item type allowed.
+ * In this case, one select box for material and label for item type is displayed,
+ *
  * @author Alexander_Kuzyakov on 26.06.2019.
  */
 public class ItemPartMaterialSelection {
+
 }
