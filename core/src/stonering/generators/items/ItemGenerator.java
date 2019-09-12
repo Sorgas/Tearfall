b49a3a610c39623374114d2ba302fa7e5c396efb
Alexander <alexander.kuzyakov@gmail.com>
2019-09-12 18:42:10 +0300

    upgrade gradle wrapper

--- core/src/stonering/generators/items/ItemGenerator.java
+++ core/src/stonering/generators/items/ItemGenerator.java
@@ -1,7 +1,7 @@
 
 package stonering.generators.items;
 
-import com.sun.xml.internal.ws.util.StringUtils;
+import com.sun.xml.ws.util.StringUtils;
 import stonering.entity.Aspect;
 import stonering.entity.crafting.ItemOrder;
 import stonering.entity.item.aspects.ItemContainerAspect;
