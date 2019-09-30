84aafe9c40a0b3a84210361e825a615b6d40dcfe
Alexander <alexander.kuzyakov@gmail.com>
2019-09-30 11:42:51 +0300

    refactor atlases enum

--- core/src/stonering/stage/renderer/AtlasesEnum.java
+++ core/src/stonering/stage/renderer/AtlasesEnum.java
@@ -4,14 +4,15 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import stonering.util.geometry.Position;
 import stonering.util.global.Logger;
+import stonering.util.global.Pair;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static stonering.stage.renderer.BatchUtil.*;
-
 /**
- * Contains all textures used for render. See {@link TileRenderer}, {@link DrawingUtil}
+ * Contains all textures used for render. See {@link TileRenderer}, {@link DrawingUtil}.
+ * Logical tile (in game model) has width, height and depth. Texture tile has width and height(height + depth).
+ * Also, for blocks, toppings are needed. Toppings has width and depth same as main tile, and reduced height.
  * TODO create sprite descriptor for key (x, y, color).
  *
  * @author Alexander on 03.08.2019.
@@ -22,18 +23,19 @@ public enum AtlasesEnum {
     units(new Texture("sprites/units.png"), true, 64, 64, 32, 6), //TODO remove topings
     buildings(new Texture("sprites/buildings.png"), true, 64, 64, 32, 6), // buildings and furniture
     ui_tiles(new Texture("sprites/ui_tiles.png"), true, 64, 64, 32, 6), // designations
-    items(new Texture("sprites/items.png"), false, 64, 64, 0, 0),
+    items(new Texture("sprites/items.png"), false, 32, 32, 0, 0),
     substrates(new Texture("sprites/substrates.png"), true, 64, 64, 32, 6), // flat plants like mosses TODO remove topings
     liquids(new Texture("sprites/liquids.png"), true, 64, 64, 32, 6);
 
     public final Texture atlas;
     public final boolean hasToppings;
-    public final Map<Position, TextureRegion> spriteCache;
     public final int WIDTH;
     public final int DEPTH;
-    public final int BLOCK_HEIGHT; // depth and
-    public final int TOPPING_BLOCK_HEIGTH;
-    public final int TOPPING_TILE_HEIGHT
+    public final int HEIGHT;
+    public final int BLOCK_HEIGHT; // depth and height
+    public final int TOPPING_BLOCK_HEIGHT; // depth and height
+    public final int FULL_TILE_HEIGHT;
+    public final Map<Pair<Integer, Integer>, TextureRegion> spriteCache;
     private Position cachePosition;
 
     AtlasesEnum(Texture texture, boolean hasToppings, int width, int depth, int height, int toppingHeight) {
@@ -42,7 +44,9 @@ public enum AtlasesEnum {
         WIDTH = width;
         DEPTH = depth;
         HEIGHT = height;
-        TOPPING_HEIGTH = toppingHeight;
+        BLOCK_HEIGHT = depth + height;
+        TOPPING_BLOCK_HEIGHT = toppingHeight + depth;
+        FULL_TILE_HEIGHT = BLOCK_HEIGHT + TOPPING_BLOCK_HEIGHT;
         spriteCache = new HashMap<>();
         cachePosition = new Position();
     }
@@ -52,15 +56,12 @@ public enum AtlasesEnum {
      * Handles atlases with no toppings.
      */
     public TextureRegion getBlockTile(int x, int y) {
-        if (!spriteCache.containsKey(cachePosition.set(x, y, 0))) {
-            int atlasY = hasToppings ? y * (BLOCK_TILE_HEIGHT) + TOPING_TILE_HEIGHT : y * (HEIGHT + DEPTH); // consider toppings or not
-            int astlasY = hasToppings ? y * (BLOCK_TILE_HEIGHT) + TOPING_TILE_HEIGHT : y * (HEIGHT + DEPTH); // consider toppings or not
-            spriteCache.put(cachePosition, new TextureRegion(atlas,
-                    x * WIDTH,
-                    atlasY,
-                    WIDTH, TILE_HEIGHT));
+        Pair<Integer, Integer> key = new Pair<>(x, y);
+        if (!spriteCache.containsKey(key)) {
+            int atlasY = y * (hasToppings ? FULL_TILE_HEIGHT + TOPPING_BLOCK_HEIGHT : BLOCK_HEIGHT); // consider toppings or not
+            spriteCache.put(key, new TextureRegion(atlas, x * WIDTH, atlasY, WIDTH, BLOCK_HEIGHT));
         }
-        return spriteCache.get(cachePosition);
+        return spriteCache.get(key);
     }
 
     public TextureRegion getBlockTile(int[] xy) {
@@ -76,21 +77,8 @@ public enum AtlasesEnum {
             Logger.RENDER.logError("Attempt to get topping from atlas without toppings.");
             return null;
         }
-        if (!spriteCache.containsKey(cachePosition.set(x, y, 1))) {
-            spriteCache.put(cachePosition, new TextureRegion(atlas,
-                    x * WIDTH,
-                    y * BLOCK_TILE_HEIGHT,
-                    WIDTH, TOPING_TILE_HEIGHT));
-        }
-        return spriteCache.get(cachePosition);
-    }
-
-    private int getAtlasY(int y) {
-        return hasToppings ?
-    }
-
-    public TextureRegion getTopingTile(int[] xy) {
-        return getTopingTile(xy[0], xy[1]);
+        Pair<Integer, Integer> key = new Pair<>(x, y);
+        spriteCache.putIfAbsent(key, new TextureRegion(atlas, x * WIDTH, y * BLOCK_HEIGHT, WIDTH, BLOCK_HEIGHT));
+        return spriteCache.get(key);
     }
 }
-
--- core/src/stonering/util/global/Pair.java
+++ core/src/stonering/util/global/Pair.java
@@ -1,5 +1,7 @@
 package stonering.util.global;
 
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
 import java.io.Serializable;
 
 /**
@@ -7,73 +9,29 @@ import java.io.Serializable;
  *
  * @author Alexander on 19.07.2018.
  */
+@Immutable
 public class Pair<K, V> implements Serializable {
-
-    /**
-     * <p>A convenience class to represent name-value pairs.</p>
-     * @since JavaFX 2.0
-     */
-
-    /**
-     * Key of this <code>Pair</code>.
-     */
     private K key;
-
-    /**
-     * Gets the key for this pair.
-     *
-     * @return key for this pair
-     */
-    public K getKey() {
-        return key;
-    }
-
-    /**
-     * Value of this this <code>Pair</code>.
-     */
     private V value;
 
-    /**
-     * Gets the value for this pair.
-     *
-     * @return value for this pair
-     */
-    public V getValue() {
-        return value;
-    }
-
-    /**
-     * Creates a new pair
-     *
-     * @param key   The key for this pair
-     * @param value The value to use for this pair
-     */
     public Pair(K key, V value) {
         this.key = key;
         this.value = value;
     }
 
-    /**
-     * <p><code>String</code> representation of this
-     * <code>Pair</code>.</p>
-     * <p>
-     * <p>The default name/value delimiter '=' is always used.</p>
-     *
-     * @return <code>String</code> representation of this <code>Pair</code>
-     */
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
     @Override
     public String toString() {
         return key + "=" + value;
     }
 
-    /**
-     * <p>Generate a hash code for this <code>Pair</code>.</p>
-     * <p>
-     * <p>The hash code is calculated using both the name and
-     * the value of the <code>Pair</code>.</p>
-     *
-     * @return hash code for this <code>Pair</code>
-     */
     @Override
     public int hashCode() {
         // name's hashCode is multiplied by an arbitrary prime number (13)
@@ -84,22 +42,6 @@ public class Pair<K, V> implements Serializable {
         return key.hashCode() * 13 + (value == null ? 0 : value.hashCode());
     }
 
-    /**
-     * <p>Test this <code>Pair</code> for equality with another
-     * <code>Object</code>.</p>
-     * <p>
-     * <p>If the <code>Object</code> to be tested is not a
-     * <code>Pair</code> or is <code>null</code>, then this method
-     * returns <code>false</code>.</p>
-     * <p>
-     * <p>Two <code>Pair</code>s are considered equal if and only if
-     * both the names and values are equal.</p>
-     *
-     * @param o the <code>Object</code> to test for
-     *          equality with this <code>Pair</code>
-     * @return <code>true</code> if the given <code>Object</code> is
-     * equal to this <code>Pair</code> else <code>false</code>
-     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
