96292d1d5af00541f7a4d4a99c5292c3f954a399
Alexander <alexander.kuzyakov@gmail.com>
2020-01-08 01:06:45 +0300

    enable entity selection with mouse

--- core/src/stonering/game/controller/controllers/EntitySelectorInputAdapter.java
+++ core/src/stonering/game/controller/controllers/EntitySelectorInputAdapter.java
@@ -4,35 +4,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import stonering.game.GameMvc;
 import stonering.game.model.EntitySelector;
 import stonering.game.model.local_map.LocalMap;
 import stonering.stage.renderer.AtlasesEnum;
 import stonering.util.geometry.Position;
+import stonering.util.global.Executor;
 import stonering.util.global.Logger;
-
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 import static com.badlogic.gdx.Input.Keys.*;
 
 /**
- * Handles button presses for entitySelector navigation. Can be disabled.
+ * Handles button presses and mouse movement for entitySelector navigation. Can be disabled.
+ * Has handler for handling clicks and selection.
  *
  * @author Alexander Kuzyakov
  */
 public class EntitySelectorInputAdapter extends InputAdapter {
     private EntitySelector selector;
     private boolean enabled;
-    private final Consumer<Position> defaultSelectHandler;
-    public Consumer<Position> selectHandler;
-
+    private final Executor defaultSelectHandler;
+    public Executor selectHandler;
+    private Position cachePosition;
+    
     public EntitySelectorInputAdapter() {
         selector = GameMvc.instance().model().get(EntitySelector.class);
         enabled = true;
         defaultSelectHandler = this::showEntityStage;
+        cachePosition = new Position();
     }
 
     @Override
@@ -40,7 +39,7 @@ public class EntitySelectorInputAdapter extends InputAdapter {
         if (!enabled) return false;
         if(keycode == Input.Keys.E) {
             Logger.INPUT.logDebug("handling E in EntitySelectorInputAdapter");
-            (selectHandler != null ? selectHandler : defaultSelectHandler).accept(selector.position); // use dynamic handler if possible
+            handleSelection();
             return true;
         }
         int offset = Gdx.input.isKeyPressed(SHIFT_LEFT) ? 10 : 1;
@@ -101,22 +100,35 @@ public class EntitySelectorInputAdapter extends InputAdapter {
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
-        Vector3 batchCoords = GameMvc.instance().view().localWorldStage.getCamera().unproject(new Vector3(screenX, screenY, 0));
-        AtlasesEnum atlas = AtlasesEnum.blocks;
-        int heightToSkip = selector.position.z * atlas.HEIGHT + (atlas.hasToppings ? atlas.TOPPING_HEIGHT : 0);
-        int x = (int) batchCoords.x / atlas.WIDTH;
-        int y = ((int) batchCoords.y - heightToSkip) / atlas.DEPTH;
-        selector.position.set(x,y, selector.position.z);
+        selector.position.set(getModelPosition(screenX, screenY));
         GameMvc.instance().model().get(LocalMap.class).normalizePosition(selector.position);
         return true;
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-//        GameMvc.instance().getView().showEntityStage();
+        selector.position.set(getModelPosition(screenX, screenY));
+        GameMvc.instance().model().get(LocalMap.class).normalizePosition(selector.position);
+        handleSelection();
+        System.out.println(selector.position);
         return true;
     }
 
+    /**
+     * Finds model position of screen point in current z-level.
+     */
+    private Position getModelPosition(int screenX, int screenY) {
+        Vector3 batchCoords = GameMvc.instance().view().localWorldStage.getCamera().unproject(new Vector3(screenX, screenY, 0));
+        AtlasesEnum atlas = AtlasesEnum.blocks;
+        int heightToSkip = selector.position.z * atlas.HEIGHT + (atlas.hasToppings ? atlas.TOPPING_HEIGHT : 0);
+        int x = (int) batchCoords.x / atlas.WIDTH;
+        int y = ((int) batchCoords.y - heightToSkip) / atlas.DEPTH;
+        return cachePosition.set(x,y, selector.position.z);
+    }
+
+    private void handleSelection() {
+        (selectHandler != null ? selectHandler : defaultSelectHandler).execute(); // use dynamic handler if possible
+    }
 
     private int charToKeycode(char character) {
         return valueOf(Character.valueOf(character).toString().toUpperCase());
@@ -126,7 +138,7 @@ public class EntitySelectorInputAdapter extends InputAdapter {
         this.enabled = enabled;
     }
 
-    private void showEntityStage(Position position) {
-        GameMvc.instance().view().showEntityStage(position);
+    private void showEntityStage() {
+        GameMvc.instance().view().showEntityStage(selector.position);
     }
 }
