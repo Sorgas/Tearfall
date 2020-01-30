2e14f4dca8c65794351c246e3ca7bab1cee651f0
Alexander <alexander.kuzyakov@gmail.com>
2020-01-30 18:54:53 +0300

    rework equipment tab

--- core/src/stonering/stage/unit/ItemWidget.java
+++ core/src/stonering/stage/unit/ItemWidget.java
@@ -1,5 +1,6 @@
 package stonering.stage.unit;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.sun.istack.NotNull;
@@ -9,21 +10,22 @@ import stonering.enums.images.DrawableMap;
 
 /**
  * Square widget, that shows icon of item.
+ * Is a stack of two images. Background image is larger than an icon image. Icon is wrapped in {@link Container} for aligning.
  *
  * @author Alexander on 29.01.2020
  */
 public class ItemWidget extends Stack {
+    private static final int ICON_SIZE = 64;
     private Image background;
     private Image itemIcon;
 
     public ItemWidget(Item item) {
         addActor(background = new Image(DrawableMap.TEXTURE.getDrawable("ui/item_slot.png")));
         if(item != null) setIcon(item);
-        setSize(64, 64);
     }
 
     private void setIcon(@NotNull Item item) {
-        addActor(itemIcon = new Image(item.getAspect(RenderAspect.class).region));
-        itemIcon.setSize(64, 64);
+        addActor(new Container<>(itemIcon = new Image(item.getAspect(RenderAspect.class).region))); // for centering icon
+        itemIcon.setSize(ICON_SIZE, ICON_SIZE);
     }
 }
--- core/src/stonering/stage/unit/UnitEquipmentTab.java
+++ core/src/stonering/stage/unit/UnitEquipmentTab.java
@@ -1,45 +1,63 @@
 package stonering.stage.unit;
 
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import stonering.entity.unit.Unit;
 import stonering.entity.unit.aspects.equipment.EquipmentAspect;
-import stonering.entity.unit.aspects.equipment.EquipmentSlot;
-import stonering.enums.images.DrawableMap;
+import stonering.util.geometry.Position;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Group with several columns of {@link ItemWidget}.
  * Container with a {@link Stack} that shows icons of a unit's equipped items, and list of hauled items.
  * Each slot image is container in own container.
  *
  * @author Alexander on 28.01.2020.
  */
-public class UnitEquipmentTab extends Container<Group> {
-    private static Map<String, Vector2> positionMap; // holds positions inside tab for different slot types.
-    private Group group;
+public class UnitEquipmentTab extends HorizontalGroup {
+    private static Map<Position, String> positionMap; // holds positions inside tab for different slot types. z is used as padding to previous widget.
+    private VerticalGroup[] columns;
 
     {
         positionMap = new HashMap<>();
-        positionMap.put("head", new Vector2(380, 700));
-        //TODO add other types
+        positionMap.put(new Position(0, 0, 186), "right gauntlet");
+        positionMap.put(new Position(0, 1, 33), "right glove");
+        positionMap.put(new Position(0, 2, 33), "right ring");
+        positionMap.put(new Position(0, 3, 33), "right hand");
+        positionMap.put(new Position(1, 1, 0), "right foot");
+        positionMap.put(new Position(2, 0, 33), "head");
+        positionMap.put(new Position(2, 1, 33), "neck");
+        positionMap.put(new Position(2, 2, 73), "chest");
+        positionMap.put(new Position(2, 3, 73), "belt");
+        positionMap.put(new Position(2, 4, 33), "legs");
+        positionMap.put(new Position(3, 0, 73), "cloak");
+        positionMap.put(new Position(3, 1, 33), "backpack");
+        positionMap.put(new Position(3, 2, 33), "left foot");
+        positionMap.put(new Position(4, 0, 186), "left gauntlet");
+        positionMap.put(new Position(4, 0, 33), "left glove");
+        positionMap.put(new Position(4, 0, 33), "left ring");
+        positionMap.put(new Position(4, 0, 33), "left hand");
     }
 
     public UnitEquipmentTab(Unit unit) {
-        setActor(group = new Group());
+        columns = new VerticalGroup[5];
         EquipmentAspect equipmentAspect = unit.getAspect(EquipmentAspect.class);
-        equipmentAspect.slots.values().stream().map(this::createSlotWidget).forEach(group::addActor);
-        setBackground(DrawableMap.TEXTURE.getDrawable("ui/equipment_background.png"));
-        size(600, 800);
+        for (int i = 0; i < 5; i++) {
+            columns[i] = new VerticalGroup();
+            fillColumn(i, equipmentAspect);
+            addActor(columns[i]);
+        }
+
     }
 
-    private ItemWidget createSlotWidget(EquipmentSlot slot) {
-        ItemWidget widget = new ItemWidget(slot.item);
-        Vector2 widgetPosition = positionMap.getOrDefault(slot.name, new Vector2(50, 50));
-        widget.setPosition(widgetPosition.x, widgetPosition.y);
-        System.out.println(widget.getPrefHeight());
-        return widget;
+    private void fillColumn(int i, EquipmentAspect aspect) {
+        positionMap.keySet().stream()
+                .filter(position -> position.x == i)
+                .forEach(position -> columns[i].addActorAt(position.y, new ItemWidget(aspect.slots.get(positionMap.get(position)).item)));
+
     }
+
 }
