71bbc3218757392d884854a31089f5cf82ecde38
Alexander <alexander.kuzyakov@gmail.com>
2020-07-22 18:06:43 +0300

    q

--- core/src/stonering/enums/time/TimeUnitEnum.java
+++ core/src/stonering/enums/time/TimeUnitEnum.java
@@ -7,11 +7,11 @@ package stonering.enums.time;
  * @author Alexander on 12.07.2019.
  */
 public enum TimeUnitEnum {
-    TICK("tick", 1), // 0/60 real second
-    MINUTE("minute", 25), // 5/12 real second 
-    HOUR("hour", 60), // 1500 ticks, 25 real seconds
-    DAY("day", 24), // 10 real minutes
-    MONTH("month", 20), // TODO make month and seasons with different lengths and names.
+    TICK("tick", 1),        // 1/60 real second
+    MINUTE("minute", 25),   // 5/12 real second 
+    HOUR("hour", 60),       // 1500 ticks, 25 real seconds
+    DAY("day", 24),         // 10 real minutes
+    MONTH("month", 20),     // TODO make month and seasons with different lengths and names.
     YEAR("year", 12);
 
     public final String NAME;
